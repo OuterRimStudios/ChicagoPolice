e1829a19e792235995973c3b3612d1ce7c8bbbae
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-10-31 14:43:04 -0500

    added avprovideo free version, added vr panorama

---
+++ Assets/ToggleVideos.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class ToggleVideos : MonoBehaviour
+{
+    public PlayableDirector directorOne;
+    public PlayableDirector directorTwo;
+    double time;
+
+    private void Start()
+    {
+        directorTwo.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(directorOne.gameObject.activeInHierarchy)
+            {
+                time = directorOne.time;
+                directorTwo.time = time;
+                directorOne.gameObject.SetActive(false);
+                directorTwo.gameObject.SetActive(true);
+                directorTwo.Play();
+            }
+            else
+            {
+                time = directorTwo.time;
+                directorOne.time = time;
+                directorTwo.gameObject.SetActive(false);
+                directorOne.gameObject.SetActive(true);
+                directorOne.Play();
+            }
+        }
+    }
+}
