3a7601f79ceb6528ac6b015682432bd502ab2282
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-11-02 10:16:22 -0500

    build with no code

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -5,39 +5,21 @@ using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
 {
-    public GameObject clip1;
-    public GameObject clip2;
+    public VideoClip clip1;
+    public VideoClip clip2;
 
     public VideoPlayer player;
-    public VideoPlayer player2;
 
     long frame;
-    long frame2;
-
-    bool toggle;
-
-    private void Start()
-    {
-        player.Play();
-        player2.Prepare();
-    }
 
     void Update()
     {
         frame = player.frame;
-        frame2 = player2.frame;
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            toggle = !toggle;
-
-            clip1.SetActive(!clip1.activeInHierarchy);
-            clip2.SetActive(!clip2.activeInHierarchy);
-
-            if(clip1.activeInHierarchy)
-                player.frame = frame2;
-            else
-                player2.frame = frame;
+            player.clip = player.clip == clip1 ? clip2 : clip1;
+            player.frame = frame;
         }
     }
 }
