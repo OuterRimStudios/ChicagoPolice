927cc5b330e891937194ec405ecd3279c07a3da0
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-11-04 18:02:34 -0600

    added avprovideo, added 2 perspective videos

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+public class VideoManager : MonoBehaviour
+{
+    public MediaPlayer playerOne;
+    public MeshRenderer sphereOne;
+    public MediaPlayer playerTwo;
+    public MeshRenderer sphereTwo;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(30f);
+        float time = playerOne.Control.GetCurrentTimeMs();
+        playerOne.Control.Pause();
+        sphereOne.enabled = false;
+        playerTwo.Control.Seek(time);
+        sphereTwo.enabled = true;
+        playerTwo.Control.Play();
+    }
+
+    //void Update()
+    //{
+    //    if (switchPerspective > 0)
+    //    {
+    //            if (playerOne.Control.IsPlaying())
+    //            {
+    //                float time = playerOne.Control.GetCurrentTimeMs();
+    //                playerOne.Control.Pause();
+    //                sphereOne.enabled = false;
+    //                playerTwo.Control.Seek(time);
+    //                sphereTwo.enabled = true;
+    //                playerTwo.Control.Play();
+    //            }
+    //            else
+    //            {
+    //                float time = playerTwo.Control.GetCurrentTimeMs();
+    //                playerTwo.Control.Pause();
+    //                sphereTwo.enabled = false;
+    //                playerOne.Control.Seek(time);
+    //                sphereOne.enabled = true;
+    //                playerOne.Control.Play();
+    //            }
+    //    }
+    //}
+}
