5b5094031cbae9feb09665c702bab0a6d8405932
hectorborges <hectorborges94@gmail.com>
2019-11-18 14:39:16 -0600

    re enabled mesh renderers

--- Assets/Scripts/VideoScene.cs
+++ Assets/Scripts/VideoScene.cs
@@ -1,14 +1,20 @@
 ﻿using RenderHeads.Media.AVProVideo;
+using UnityEngine;
 public class VideoScene : BaseScene
 {
     public MediaPlayer mediaPlayer;
+    public GameObject videoSphere;
     public override void StartScene()
     {
+        Debug.LogError("Start Scene: " + name);
+        videoSphere.SetActive(true);
         mediaPlayer.Play();
     }
 
     public override void EndScene()
     {
-        mediaPlayer.Stop();
+        Debug.LogError("End Scene Scene: " + name);
+        mediaPlayer.Pause();
+        videoSphere.SetActive(false);
     }
 }
