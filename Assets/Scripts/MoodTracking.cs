cc30d000e5db097cfb3b3d16239226f06d850821
PeterHoppers <peterhoppe341@gmail.com>
2019-11-18 11:34:11 -0600

    Init MoodTracking
    Because Hector wants me to push this

---
+++ Assets/Scripts/MoodTracking.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+public class MoodTracking : MonoBehaviour
+{
+    byte moodIndex = 6;
+    MediaPlayer currentMediaPlayer;
+    const double USER_DEAD_ZONE = .2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        //ChicagoSceneTransition.OnSceneStarted += SetMediaType;
+    }
+
+    void OnDisable()
+    {
+        //ChicagoSceneTransition.OnSceneStarted -= SetMediaType;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x > USER_DEAD_ZONE)
+        {
+            Debug.Log("Moving Right");
+            float secondsSinceStart = currentMediaPlayer.Control.GetCurrentTimeMs() * 1000;
+            moodIndex++;
+
+        }
+        else if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x < -USER_DEAD_ZONE)
+        {
+            Debug.Log("Moving Left");
+            moodIndex--;
+        }
+    }
+
+    void SetMediaType(/*BaseScene baseScene*/)
+    {
+        /*if (baseScene.GetType() == typeof(VideoScene))
+        {
+            currentMediaPlayer = baseScene.GetMediaPlayer();*/
+        
+        //}
+    }
+
+    
+
+    //Movement for the Slider
+    //UI for the the Slider
+    //Time sense 
+}
