3cf33a09185bf447ff56ee856206c36fee974430
PeterHoppers <peterhoppe341@gmail.com>
2019-11-19 13:29:11 -0600

    Seperated Tracking Info away from the UI
    Hopefullt this makes it reuseable for Sam's UI as well.

--- Assets/Scripts/MoodTracking.cs
+++ Assets/Scripts/MoodTracking.cs
@@ -6,29 +6,9 @@ using RenderHeads.Media.AVProVideo;
 
 public class MoodTracking : MonoBehaviour
 {
-    public Color neutralColor;
-    public Color tenseColor;
-    public Color relaxedColor;
-
-    public Slider moodSlider;
-    public Image spotImage;
-    public RectTransform middleBackground;
     MediaPlayer currentMediaPlayer;
 
-    List<MoodInfo> moodInfos = new List<MoodInfo>();
-
-    [SerializeField]
-    double USER_DEAD_ZONE = .2;
-
-    byte moodIndex = 6;
-    bool canUpdateSlider = true;
-
-    // Start is called before the first frame update
-    void Start()
-    {        
-        UpdateUI();
-        SetUI();
-    }
+    List<MoodInfo> moodInfos = new List<MoodInfo>();   
 
     void OnEnable()
     {
@@ -38,41 +18,7 @@ public class MoodTracking : MonoBehaviour
     void OnDisable()
     {
         ChicagoSceneTransition.OnSceneStarted -= SetMediaType;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        OVRInput.Update();
-
-        if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x > USER_DEAD_ZONE && canUpdateSlider || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            StartCoroutine(InputCooldown());
-
-            if (moodIndex < moodSlider.maxValue)
-                moodIndex++;
-
-            UpdateUI();
-            CreateMoodLine();
-        }
-        else if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x < -USER_DEAD_ZONE && canUpdateSlider || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            StartCoroutine(InputCooldown());
-
-            if (moodIndex > moodSlider.minValue)
-                moodIndex--;
-
-            UpdateUI();
-            CreateMoodLine();
-        }
-    }
-
-    IEnumerator InputCooldown()
-    {
-        canUpdateSlider = false;
-        yield return new WaitForSeconds(.25f);
-        canUpdateSlider = true;
-    }
+    }    
 
     void SetMediaType(BaseScene baseScene)
     {
@@ -82,57 +28,16 @@ public class MoodTracking : MonoBehaviour
         }
     }
 
-    void CreateMoodLine()
+    public void CreateMoodLine(Slider sliderValue)
     {
-        moodInfos.Add(new MoodInfo(currentMediaPlayer?.Control.GetCurrentTimeMs() * 1000 ?? 10, moodIndex));
-    }
-
-    void SetUI()
-    {        
-        float sliderWidth = moodSlider.GetComponent<RectTransform>().sizeDelta.x;
-        float incrementValue = sliderWidth / (moodSlider.maxValue - moodSlider.minValue);
-
-        moodSlider.handleRect.sizeDelta = new Vector2(incrementValue, 0);
-
-        float startingValue = -(sliderWidth / 2);
-        for (int i = 0; i < moodSlider.maxValue; i++)
-        {
-            var spawnedRectTransform = Instantiate(spotImage, middleBackground.transform).GetComponent<RectTransform>();
-            spawnedRectTransform.localPosition = new Vector2(startingValue, spotImage.GetComponent<RectTransform>().localPosition.y);
-            spawnedRectTransform.sizeDelta = new Vector2(incrementValue, moodSlider.handleRect.rect.height);
-            startingValue += incrementValue;
-        }
-
-
-    }
-
-    void UpdateUI()
-    {        
-        moodSlider.value = moodIndex;
-
-        if (moodIndex > 6)
-        {
-            moodSlider.handleRect.GetComponent<Image>().color = tenseColor;
-        }
-        else if (moodIndex == 6)
-        {
-            moodSlider.handleRect.GetComponent<Image>().color = neutralColor;
-        }
-        else
-        {
-            moodSlider.handleRect.GetComponent<Image>().color = relaxedColor;
-        }
-
-        /*
-        float handleX = moodSlider.handleRect.localPosition.x;
-        middleBackground.sizeDelta = new Vector2(Mathf.Abs(handleX), middleBackground.sizeDelta.y);
-        middleBackground.localPosition = new Vector2(handleX / 2, middleBackground.localPosition.y);*/           
+        moodInfos.Add(new MoodInfo(currentMediaPlayer?.Control.GetCurrentTimeMs() * 1000 ?? -1, sliderValue?.value ?? -1));
     }
 }
 
-internal class MoodInfo{
+internal class MoodInfo
+{
     public double time;
-    public byte mood;
+    public float mood;
 
-    public MoodInfo(double time, byte mood) { this.time = time; this.mood = mood; }
+    public MoodInfo(double time, float mood) { this.time = time; this.mood = mood; }
 }
