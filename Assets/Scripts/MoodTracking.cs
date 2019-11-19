b1f354e0f21325c61464b3fc3a7b50fb5e32c064
PeterHoppers <peterhoppe341@gmail.com>
2019-11-19 12:01:25 -0600

    Added bubbles and color changing
    Changed the UI to work like more of a fill in bubble,  rather than a bar.

--- Assets/Scripts/MoodTracking.cs
+++ Assets/Scripts/MoodTracking.cs
@@ -6,7 +6,12 @@ using RenderHeads.Media.AVProVideo;
 
 public class MoodTracking : MonoBehaviour
 {
+    public Color neutralColor;
+    public Color tenseColor;
+    public Color relaxedColor;
+
     public Slider moodSlider;
+    public Image spotImage;
     public RectTransform middleBackground;
     MediaPlayer currentMediaPlayer;
 
@@ -20,8 +25,9 @@ public class MoodTracking : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {        
         UpdateUI();
+        SetUI();
     }
 
     void OnEnable()
@@ -81,13 +87,46 @@ public class MoodTracking : MonoBehaviour
         moodInfos.Add(new MoodInfo(currentMediaPlayer?.Control.GetCurrentTimeMs() * 1000 ?? 10, moodIndex));
     }
 
+    void SetUI()
+    {        
+        float sliderWidth = moodSlider.GetComponent<RectTransform>().sizeDelta.x;
+        float incrementValue = sliderWidth / (moodSlider.maxValue - moodSlider.minValue);
+
+        moodSlider.handleRect.sizeDelta = new Vector2(incrementValue, 0);
+
+        float startingValue = -(sliderWidth / 2);
+        for (int i = 0; i < moodSlider.maxValue; i++)
+        {
+            var spawnedRectTransform = Instantiate(spotImage, middleBackground.transform).GetComponent<RectTransform>();
+            spawnedRectTransform.localPosition = new Vector2(startingValue, spotImage.GetComponent<RectTransform>().localPosition.y);
+            spawnedRectTransform.sizeDelta = new Vector2(incrementValue, moodSlider.handleRect.rect.height);
+            startingValue += incrementValue;
+        }
+
+
+    }
+
     void UpdateUI()
     {        
         moodSlider.value = moodIndex;
 
+        if (moodIndex > 6)
+        {
+            moodSlider.handleRect.GetComponent<Image>().color = tenseColor;
+        }
+        else if (moodIndex == 6)
+        {
+            moodSlider.handleRect.GetComponent<Image>().color = neutralColor;
+        }
+        else
+        {
+            moodSlider.handleRect.GetComponent<Image>().color = relaxedColor;
+        }
+
+        /*
         float handleX = moodSlider.handleRect.localPosition.x;
         middleBackground.sizeDelta = new Vector2(Mathf.Abs(handleX), middleBackground.sizeDelta.y);
-        middleBackground.localPosition = new Vector2(handleX / 2, middleBackground.localPosition.y);
+        middleBackground.localPosition = new Vector2(handleX / 2, middleBackground.localPosition.y);*/           
     }
 }
 
