9e6f44be5d5ea1714c654eb8a9d8d8e2ce8506c6
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-12-05 15:51:28 -0600

    Corrected and Completed UI Pointing

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour
+{
+    public Transform raycastPosition;
+    public Transform handPoint;
+    public LayerMask interactionLayer;
+    LineRenderer line;
+    Selectable selectable;
+    public GraphicRaycaster graphicRaycaster;
+    EventSystem eventSystem;
+    PointerEventData pointer;
+
+    private void OnEnable()
+    {
+        eventSystem = EventSystem.current;
+        OVRInputManager.OnButtonDown += OnButtonDown;
+    }
+
+    private void OnDisable()
+    {
+        OVRInputManager.OnButtonDown -= OnButtonDown;
+    }
+
+    private void OnButtonDown(OVRInput.Button key)
+    {
+        if (key == OVRInput.Button.SecondaryIndexTrigger && selectable != null)
+        {
+            BaseEventData data = new BaseEventData(eventSystem);
+            Debug.LogError($"{selectable.gameObject.name} has been clicked");
+            ExecuteEvents.Execute(selectable.gameObject, data, ExecuteEvents.submitHandler);
+        }
+    }
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        CastRay();
+    }
+
+    void CastRay()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(raycastPosition.position, raycastPosition.forward);
+
+        line.enabled = true;
+        line.SetPosition(0, raycastPosition.position);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, interactionLayer))
+        {
+            if (hit.transform.tag.Equals("UI"))
+            {
+                line.SetPosition(1, hit.point);
+                selectable = hit.transform.GetComponent<Selectable>();
+
+                if (selectable == null)
+                {
+                    Debug.LogError($"{hit.transform.gameObject.name} has no selectable");
+                }
+                else if(selectable.gameObject != null)
+                    Debug.LogError($"{selectable.gameObject.name} has been hovered over");
+            }
+        }
+        else
+        {
+            line.SetPosition(1, ray.GetPoint(20));
+            selectable = null;
+        }
+    }
+}
