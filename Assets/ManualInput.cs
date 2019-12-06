9079d198990c9d3a27eaec20072d4cd3fbb995c7
PeterHoppers <peterhoppe341@gmail.com>
2019-12-06 10:45:37 -0600

    Didn't set it up correctly
    Should have given the correct public variables.

--- Assets/ManualInput.cs
+++ Assets/ManualInput.cs
@@ -1,13 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+﻿using UnityEngine;
 
 public class ManualInput : MonoBehaviour
 {
-    public Keypad userId;
     public TextToggle groupId;
+    public Keypad userId;   
 
     public GameObject[] gosManualInput;
 
