2d24ad02fec84799da22cfc4484d170df52c8909
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-12-13 11:49:24 -0600

    made the mood bar follow the camera

--- Assets/Scripts/Scenes/GameObjectScene.cs
+++ Assets/Scripts/Scenes/GameObjectScene.cs
@@ -2,13 +2,22 @@
 
 public class GameObjectScene : BaseScene
 {
+    [Tooltip("The transform that this object will rotate to center on")]
+    public Transform centerTarget;
     public override void StartScene()
     {
+        //CenterView();
         gameObject.SetActive(true);
     }
 
     public override void EndScene()
     {
         gameObject.SetActive(false);
+    }
+
+    void CenterView()
+    {
+        if(centerTarget)
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, centerTarget.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 }
--- Assets/Scripts/StayVisible.cs
+++ Assets/Scripts/StayVisible.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class StayVisible : MonoBehaviour
 {
     public Transform target;
-    public float followSpeed = 3f;
-    public float rotationSpeed = 3f;
-
-    Transform reference;
 
     void Update()
     {
         if (target)
         {
-            reference.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only Y axis, meaning right & left turns.
-
-            Vector3 v3_Dir = transform.position - target.position;
-            float f_AngleBetween = Vector3.Angle(reference.forward, v3_Dir);
-            transform.RotateAround(target.position, Vector3.up, f_AngleBetween);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, target.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         }
     }
 }
