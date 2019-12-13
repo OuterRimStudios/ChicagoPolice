4fa6b1a568a14a4ac421b3f5a277c565d5357c3b
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-12-13 09:29:52 -0600

    made some UI changes, added the stay visible script to the mood slider

---
+++ Assets/Scripts/StayVisible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayVisible : MonoBehaviour
+{
+    public Transform target;
+    public float followSpeed = 3f;
+    public float rotationSpeed = 3f;
+
+    Transform reference;
+
+    void Update()
+    {
+        if (target)
+        {
+            reference.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only Y axis, meaning right & left turns.
+
+            Vector3 v3_Dir = transform.position - target.position;
+            float f_AngleBetween = Vector3.Angle(reference.forward, v3_Dir);
+            transform.RotateAround(target.position, Vector3.up, f_AngleBetween);
+        }
+    }
+}
